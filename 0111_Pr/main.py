91075d164d272930013f2aa7195a10830f42fb63
kirillsev1 <kirillprih47@gmail.com>
2022-11-09 19:19:14 +0300

    Second Homework 09-11-2022

--- 0111_Pr/main.py
+++ 0111_Pr/main.py
@@ -13,10 +13,15 @@ class Triangle:
             first: int - init first side.
             second: int - init second side.
             third: int - init third side.
+
+        Raises:
+            ValueError: Exception - if sides in allowed values.
         """
         self.first = first
         self.second = second
         self.third = third
+        if not self.sides_check():
+            raise ValueError
 
     def perimeter(self):
         """Method evaluates triangles perimeter.
@@ -35,6 +40,17 @@ class Triangle:
         half = (self.first + self.second + self.third) / 2
         return round((half * (half - self.first) * (half - self.second) * (half - self.third)) ** 0.5, 2)
 
+    def sides_check(self):
+        """Method checks triangle for existence.
+
+        Returns:
+            triangle_existence: bool - if sum of 2 sides larger last one.
+        """
+        side1 = self.first
+        side2 = self.second
+        side3 = self.third
+        return side1 + side2 > side3 and side3 + side1 > side2 and side3 + side2 > side1
+
 
 class Circle:
     """Class finds circles square and perimeter."""
@@ -44,10 +60,13 @@ class Circle:
 
         Args:
             radius: int - init radius.
+
+        Raises:
+            ValueError: Exception - if radius in allowed values.
         """
         self.radius = radius
-        if self.radius <= 0:
-            return None
+        if not self.radius_check():
+            raise ValueError
 
     def length(self):
         """Method which finds circles length.
@@ -64,3 +83,11 @@ class Circle:
             square: float - math result rounds up to 2 numbers after dot(square).
         """
         return round(math.pi * (self.radius ** 2), 2)
+
+    def radius_check(self):
+        """Method checks radius for existence.
+
+        Returns:
+            radius_existence: if radius lower or equals 0.
+        """
+        return self.radius <= 0
