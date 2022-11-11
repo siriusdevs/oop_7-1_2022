2d8ce5602f83e38df9cf7245baf9e7442e66678d
vecherninanika <113093677+vecherninanika@users.noreply.github.com>
2022-11-11 12:18:38 +0300

    Исправлены ошибки в classtriangle.py

--- classtriangle.py
+++ classtriangle.py
@@ -30,8 +30,6 @@ class Triangle:
         Returns:
             float: the area of circle
         """
-        p = (self.side1 + self.side2 + self.side3) / 2
+        p = self.perimeter() / 2
         area_2 = p * (p - self.side1) * (p - self.side2) * (p - self.side3)
-        for i in range(int(area_2 / 2)):
-            if i ** 2 == area_2:
-                return i
+        return area_2 ** 0.5
