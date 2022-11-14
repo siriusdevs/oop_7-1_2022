f75a463ca5ce37b842b50583b3e618822522a436
n1nekto <ad.moxito@mail.ru>
2022-11-14 12:05:18 +0300

    Final version edited

--- test_dz1_oop.py
+++ test_dz1_oop.py
@@ -2,12 +2,14 @@
 import pytest
 from dz1_oop import Circle, Triangle
 
-test_triangle_parametrs = [(1, 2, 3)]
+
+test_triangle_parametrs = [(1, 2, 3), (2, 3, 4)]
 
 
 @pytest.mark.parametrize('side1, side2, side3', test_triangle_parametrs)
 def test_triangle(side1: int, side2: int, side3: int) -> None:
     """Test for it.
+
     Args:
         side1 (float): first side of triangle.
         side2 (float): second side of triangle.
@@ -25,68 +27,68 @@ test_circle_parametrs = [(1), (2), (3)]
 @pytest.mark.parametrize('radius', test_circle_parametrs)
 def test_circle(radius: int) -> None:
     """Test for it.
+
     Args:
         radius (int): circle's radius.
     """
-    circle = Circle(radius)
-    assert circle.radius == radius
+    assert Circle(radius).radius == radius
 
 
-test_triangle_perimeter = [(1, 2, 3, 6)]
+test_triangle_perimeter = [(1, 2, 3, 6), (2, 3, 4, 9)]
 
 
 @pytest.mark.parametrize('side1, side2, side3, expect', test_triangle_perimeter)
 def test_perimeter(side1: int, side2: int, side3: int, expect: int) -> None:
     """Test for it.
+
     Args:
         side1 (float): first side of triangle.
         side2 (float): second side of triangle.
         side3 (float): third side of triangle.
         expect (float): triangle's perimeter.
     """
-    triangle = Triangle(side1, side2, side3)
-    assert triangle.perimeter() == expect
+    assert Triangle(side1, side2, side3).perimeter() == expect
 
 
-test_triangle_square = [(3, 4, 5, 77.77)]
+test_triangle_square = [(3, 4, 5, 77.77), (1, 2, 3, 18.97)]
 
 
 @pytest.mark.parametrize('side1, side2, side3, expect', test_triangle_square)
 def test_square(side1: int, side2: int, side3: int, expect: float) -> None:
     """Test for it.
+
     Args:
         side1 (float): first side of triangle.
         side2 (float): second side of triangle.
         side3 (float): third side of triangle.
         expect (float): triangle's square.
     """
-    triangle = Triangle(side1, side2, side3)
-    assert triangle.square() == expect
+    assert Triangle(side1, side2, side3).square() == expect
 
 
-test_circle_length = [(2, 12.57)]
+test_circle_length = [(2, 12.57), (3, 18.85)]
 
 
 @pytest.mark.parametrize('radius, expect', test_circle_length)
 def test_length(radius: int, expect: float) -> None:
     """Test for it.
+
     Args:
         radius (float): circle's radius.
         expect (float): circle's length.
     """
-    circle = Circle(radius)
-    assert circle.length() == expect
+    assert Circle(radius).length() == expect
 
 
-test_circle_square = [(3, 28.27)]
+test_circle_square = [(3, 28.27), (5, 78.54)]
 
 
 @pytest.mark.parametrize('radius, expect', test_circle_square)
 def test_c_square(radius: int, expect: float) -> None:
     """Test for it.
+
     Args:
         radius (float): circle's radius.
         expect (float): circle's length.
     """
-    circle = Circle(radius)
-    assert circle.square() == expect
+    assert Circle(radius).square() == expect
