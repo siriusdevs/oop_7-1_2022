b3920618fe31749cab8ef03357613e8c45d64f58
annashutova <annashut@inbox.ru>
2022-11-15 14:52:42 +0300

    Added validation for figures

--- hw1/figures.py
+++ hw1/figures.py
@@ -2,6 +2,22 @@
 from math import sqrt, pi
 
 
+class InvalidFigureError(Exception):
+    """Exception raised for invalid attributes of figures."""
+
+    def __init__(self, message: str) -> None:
+        """Error initialization.
+
+        Parameters:
+            message : str - output message of the error
+        """
+        self.message = message
+
+    def __str__(self) -> str:
+        """Return formated error message."""
+        return 'InvalidFigureError: {0}'.format(self.message)
+
+
 class Triangle:
     """Representation of the triangle.
 
@@ -18,10 +34,15 @@ class Triangle:
             side1 : int - first side of the triangle
             side2 : int - second side of the triangle
             side3 : int - third side of the triangle
+
+        Raises:
+            InvalidFigureError: if is_valid returns False
         """
         self.side1 = side1
         self.side2 = side2
         self.side3 = side3
+        if not self.is_valid():
+            raise InvalidFigureError('Triangle can not be built with these sides.')
 
     def perimeter(self) -> int:
         """Evaluate perimeter of the triangle using Heron's formula."""
@@ -31,6 +52,18 @@ class Triangle:
         """Evaluate area of the triangle and round the number to 2 decimal places."""
         semi_p = self.perimeter() / 2
         return round(sqrt(semi_p * (semi_p - self.side1) * (semi_p - self.side2) * (semi_p - self.side3)), 2)
+
+    def is_valid(self) -> bool:
+        """Check condition of triabgles existence."""
+        sides = sorted([self.side1, self.side2, self.side3])
+        if sides[2] >= sides[0] + sides[1]:
+            return False
+        for side in sides:
+            if not isinstance(side, (int, float)):
+                return False
+            if side <= 0:
+                return False
+        return True
 
 
 class Circle:
@@ -46,8 +79,13 @@ class Circle:
 
         Parameters:
             radius : int - radius of the circle
+
+        Raises:
+            InvalidFigureError: if is_valid returns False
         """
         self.radius = radius
+        if not self.is_valid():
+            raise InvalidFigureError('Circle can not be built with this radius.')
 
     def length(self) -> float:
         """Evaluate length of the circle and round the number to 2 decimal places."""
@@ -56,3 +94,10 @@ class Circle:
     def area(self) -> float:
         """Evaluate area of the circle and round the number to 2 decimal places."""
         return round(pi * (self.radius ** 2), 2)
+
+    def is_valid(self):
+        """Check condition of circle`s existence."""
+        if not isinstance(self.radius, (int, float)):
+            return False
+        if self.radius <= 0:
+            return False
