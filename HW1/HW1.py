25a658f8b2341c3547cb7a2e99ecfe6b798b037f
Nirtkor <itnirtkor@gmail.com>
2022-12-14 02:02:20 +0300

    fixed for flake8

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,17 +1,15 @@
-"""module for calculating the characteristics (area, etc.) of a circle and a triangle
-"""
+"""Module for calculating the characteristics (area, etc.) of a circle and a triangle."""
 from typing import Union
 from math import sqrt, pi
 
 
 class TriangleException(Exception):
-    """a class with a custom error for the triangle
-    """
+    """A class with a custom error for the triangle."""
     def __init__(self, message):
-        """init msg for triangle raise
+        """Init msg for triangle raise.
 
         Args:
-            message (str): _description_
+            message (str): _description_.
         """
         self.message = message
 
@@ -20,8 +18,7 @@ class TriangleException(Exception):
 
 
 class CircleException(Exception):
-    """a class with a custom error for the circle
-    """
+    """A class with a custom error for the circle."""
     def __init__(self, message):
         self.message = message
 
@@ -30,31 +27,31 @@ class CircleException(Exception):
 
 
 class Triangle:
-    """a class for creating and calculating triangle properties"""
+    """A class for creating and calculating triangle properties."""
 
     def __init__(self, side_a: Union[float, int], side_b: Union[float, int], side_c: Union[float, int]):
-        """Creating a triangle
+        """Creating a triangle.
 
         Args:
-            a (Union[float, int]): first side of the triangle
-            b (Union[float, int]): second side of the triangle
-            c (Union[float, int]): third side of the triangle
-            sides (list): the list of sides of the triangle, includes a[0], b[1], c[2]
+            side_a (Union[float, int]): first side of the triangle
+            side_b (Union[float, int]): second side of the triangle
+            side_c (Union[float, int]): third side of the triangle
+            sides (list): the list of sides of the triangle, includes a[0], b[1], c[2].
         """
         self.__sides = [side_a, side_b, side_c]
         self.checker(self.sides)
 
     def checker(self, sides):
-        """Error checker for triangle
+        """Error checker for triangle.
 
         Args:
-            sides (list): the list of sides of the triangle, includes a[0], b[1], c[2]
+            sides (list): the list of sides of the triangle, includes a[0], b[1], c[2].
 
         Raises:
             TriangleException: does not match the numeric data type
             TriangleException: checks the number of sides in a triangle
             TriangleException: does not conform to the rules of the triangle
-            TriangleException: negative numbers cannot be the length of a side
+            TriangleException: negative numbers cannot be the length of a side.
         """
         if not all([isinstance(side, (float, int)) for side in sides]):
             raise TriangleException("Неверный формат данных")
@@ -67,57 +64,57 @@ class Triangle:
 
     @property
     def sides(self):
-        """getter for sides"""
+        """Getter for sides."""
         return self.__sides
 
     @sides.setter
     def sides(self, sides):
-        """setter for sides
+        """Setter for sides.
 
         Args:
-            sides (list): the list of sides of the triangle, includes a[0], b[1], c[2]
+            sides (list): the list of sides of the triangle, includes a[0], b[1], c[2].
         """
         self.checker(sides)
         self.__sides = sides
 
     def perimetr(self):
-        """calculating the rounded perimeter of a triangle
+        """Calculating the rounded perimeter of a triangle.
 
         Returns:
-            float: perimetr of triangle
+            float: perimetr of triangle.
         """
         return round((self.__sides[0] + self.__sides[1] + self.__sides[2]), 2)
 
     def square(self):
-        """calculating the rounded area of a triangle
+        """Calculating the rounded area of a triangle.
 
         Returns:
-            float: square of triangle
+            float: square of triangle.
         """
         p = self.perimetr() * 0.5
         return round((sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2]))), 2)
 
 
 class Circle:
-    """a class for creating and calculating circle properties"""
+    """A class for creating and calculating circle properties."""
     def __init__(self, radius: Union[float, int]):
-        """Creating a circle
+        """Creating a circle.
 
         Args:
-            radius (Union[float, int]): radius of a circle
+            radius (Union[float, int]): radius of a circle.
         """
         self.__radius = radius
         self.checker(self.radius)
 
     def checker(self, radius):
-        """Error checker for circle
+        """Error checker for circle.
 
         Args:
-            radius (Union[float, int]): radius of a circle
+            radius (Union[float, int]): radius of a circle.
 
         Raises:
             CircleException: check for a negative number
-            CircleException: data format check
+            CircleException: data format check.
         """
         if not isinstance(radius, Union[float, int]):
             raise CircleException("Неверный формат данных")
@@ -126,31 +123,31 @@ class Circle:
 
     @property
     def radius(self):
-        """getter for radius"""
+        """Getter for radius."""
         return self.__radius
 
     @radius.setter
     def radius(self, radius: Union[float, int]):
-        """setter for radius
+        """Setter for radius.
 
         Args:
-            radius (Union[float, int]): radius of a circle
+            radius (Union[float, int]): radius of a circle.
         """
         self.checker(radius)
         self.__radius = radius
 
     def circumference(self):
-        """rounded circle length calculation
+        """Rounded circle length calculation.
 
         Returns:
-            float: circle length
+            float: circle length.
         """
         return round((2 * pi * self.__radius), 2)
 
     def square(self):
-        """rounded circle area calculation
+        """Rounded circle area calculation.
 
         Returns:
-            float: circle area
+            float: circle area.
         """
         return round((pi * self.__radius ** 2), 2)
--- HW1/test_HW1.py
+++ HW1/test_HW1.py
@@ -8,7 +8,7 @@ side_transfer_test = (3, 5, 7), (5.0, 4.0, 8.0), (6.5, 8, 4)
 
 @pytest.mark.parametrize('side_a, side_b, side_c', side_transfer_test)
 def test_side_transfer(side_a: Union[float, int], side_b: Union[float, int], side_c: Union[float, int]):
-    """Testing raise checker for triangle
+    """Testing raise checker for triangle.
 
     Args:
         side_a (Union[float, int]): the first side of triangle
@@ -34,7 +34,6 @@ def test_error_zero_triangle():
 @pytest.mark.xfail(raises=Exception)
 def test_error_value_triangle():
     """Data type test."""
-
     with pytest.raises(TriangleException):
         Triangle(5, "Koshmar", 6.9)
 
@@ -47,11 +46,11 @@ triangle_square = [(Triangle(5, 5, 8), 12.0), (Triangle(1, 1, 1), 0.43), (Triang
 
 @pytest.mark.parametrize('triangle, expect', triangle_square)
 def test_triangle_square(triangle: Triangle, expect: float):
-    """Checking the correct calculation of the area of a triangle
+    """Checking the correct calculation of the area of a triangle.
 
     Args:
         triangle (Triangle): the values of the sides of the triangle
-        expect (float): test area
+        expect (float): test area.
     """
     assert triangle.square() == expect
 
@@ -61,11 +60,11 @@ triangle_perimetr = [(Triangle(5, 5.0, 5), 15.0), (Triangle(9, 4.5, 5), 18.5), (
 
 @pytest.mark.parametrize('triangle, expect', triangle_perimetr)
 def test_triangle_perimetr(triangle: Triangle, expect: float):
-    """Checking the correct calculation of the perimetr of a triangle
+    """Checking the correct calculation of the perimetr of a triangle.
 
     Args:
         triangle (Triangle): the values of the sides of the triangle
-        expect (float): perimetr value
+        expect (float): perimetr value.
     """
     assert triangle.perimetr() == expect
 
@@ -89,11 +88,11 @@ circle_len_test = [(Circle(10), 62.83), (Circle(16), 100.53)]
 
 @pytest.mark.parametrize('circle, expect', circle_len_test)
 def test_circle_len(circle: Circle, expect: float):
-    """Testing the length of the circle
+    """Testing the length of the circle.
 
     Args:
         circle (Circle): circle parameters
-        expect (float): length value
+        expect (float): length value.
     """
     assert circle.circumference() == expect
 
@@ -103,10 +102,10 @@ circle_square_test = [(Circle(10), 314.16), (Circle(16), 804.25)]
 
 @pytest.mark.parametrize('circle, expect', circle_square_test)
 def test_circle_square(circle: Circle, expect: float):
-    """Testing the square of the circle
+    """Testing the square of the circle.
 
     Args:
         circle (Circle): circle parameters
-        expect (float): square value
+        expect (float): square value.
     """
     assert circle.square() == expect
