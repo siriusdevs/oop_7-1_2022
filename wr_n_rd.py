91807bc3f84705aff31ca0859ab2fa9b6b3d0336
radugaboost <113132811+radugaboost@users.noreply.github.com>
2022-12-20 22:47:54 +0300

    Update

--- wr_n_rd.py
+++ wr_n_rd.py
@@ -1,11 +1,11 @@
 """Readers and writers."""
-from threading import Thread, Condition, Event
+from threading import Thread, Condition, Lock
 from time import sleep
 from random import randint
 
 
-TEXT = ['Aboba', 'Kokos', 'Kakat']
-current_letter = ''
+TEXT = ['Aboba', 'Kokos', 'Cond']
+CUR_LETTER = ''
 
 
 class Reader(Thread):
@@ -22,9 +22,10 @@ class Reader(Thread):
 
     def run(self):
         """Run method of reader."""
-        global event
-        while event.wait():
-            print('Reader {0} read letter {1}'.format(self.name, current_letter))
+        while True:
+            with cond:
+                cond.wait()
+                print('Reader {0} read letter {1}'.format(self.name, CUR_LETTER))
 
 
 class Writer(Thread):
@@ -44,33 +45,28 @@ class Writer(Thread):
 
     def run(self):
         """Run method of writer."""
-        global cond
-        global event
-        global current_letter
-        with cond:
-            cond.wait()
-            text = TEXT[randint(0, len(TEXT) - 1)]
-            print('Writer {0} starting writing'.format(self.name))
-            res = text
-            while text:
-                letter, text = text[:1], text[1:]
-                event.set()
-                current_letter = letter
-                print('Writer {0} wrote {1}'.format(self.name, letter))
-                event.clear()
-                sleep(randint(*Writer.TIMEOUT_WRITING))
+        global CUR_LETTER
+        while True:
+            with lock:
+                print('Writer {0} starting writing'.format(self.name))
+                text = TEXT[randint(0, len(TEXT) - 1)]
+                res = text
+                while text:
+                    letter, text = text[:1], text[1:]
+                    CUR_LETTER = letter
+                    print('Writer {0} wrote {1}'.format(self.name, letter))
+                    with cond:
+                        cond.notify_all()
+                    sleep(randint(*Writer.TIMEOUT_WRITING))
             print('Writer {0} has wrote {1}'.format(self.name, res))
             print('Writer {0} goes to sleep'.format(self.name))
             sleep(randint(*Writer.TIME_OF_SLEEP))
 
 
 if __name__ == "__main__":
-    cond = Condition()
-    event = Event()
+    cond = Condition(lock=Lock())
+    lock = Lock()
     for nums in range(10):
         Reader(str(nums)).start()
-    while True:
-        for num in range(2):
-            Writer(str(num)).start()
-        with cond:
-            cond.notify_all()
+    for num in range(2):
+        Writer(str(num)).start()
