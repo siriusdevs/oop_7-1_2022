60bdddb141fd24325c3e56d4849d7e56156d7d55
Max2288 <kennySRU@ya.ru>
2022-12-20 23:25:33 +0300

    Changed solution

--- readers_and_writers.py
+++ readers_and_writers.py
@@ -1,10 +1,9 @@
 """Solving readers and writers problem."""
-from threading import Thread, Condition, Event
+from threading import Thread, Condition, Lock
 from time import sleep
 from faker import Faker
 from random import randint
 
-global string_for_people
 string_for_people = ''
 
 
@@ -22,60 +21,59 @@ class Reader(Thread):
 
     def run(self):
         """Main reader's function that start him."""
-        global event
-        while event.wait():
-            print('{0} is reading!'.format(self.name))
-            print("Current title: ", string_for_people)
+        global condition
+        while True:
+            with condition:
+                condition.wait()
+                print('{0} is reading!'.format(self.name))
+                print("Current title: ", string_for_people)
 
 
 class Writer(Thread):
     """Representation of writer."""
 
-    TIMEOUT_WRITING = (2, 4)
+    TIMEOUT_WRITING = (4, 6)
+    SLEEPING_TIME = (2, 4)
+    TEXTS = ["Cъешь этих мягких французких булок", "Не ешь эти булки, они отравлены"]
 
-    def __init__(self, name: str, text: str) -> None:
+    def __init__(self, name: str) -> None:
         """Initialization method.
 
         Args:
             name (str): writer's name.
-            text (str): writer's text.
         """
         super().__init__(name=name)
         self.name = name
-        self.text = text
 
     def run(self):
         """Main writer's function that start him."""
-        global condition
-        global event
-        global string_for_people
-        string_copy = string_for_people
-        with condition:
-            condition.wait()
-            while self.text:
-                letter = self.text[:1]
-                string_for_people += letter
-                self.text = self.text[1:]
-                event.set()
-                print("Writer {0} wrote {1}.".format(self.name, letter))
-                event.clear()
-                sleep(randint(*Writer.TIMEOUT_WRITING))
-            print("Writer {0} has wrote {1}.".format(self.name, string_for_people))
-            print('Writer {0} goes to sleep'.format(self.name))
-            string_for_people = string_copy
+        global condition, lock, string_for_people
+        while True:
+            with lock:
+                text_copy = Writer.TEXTS[randint(0, len(Writer.TEXTS) - 1)]
+                while text_copy:
+                    letter = text_copy[:1]
+                    string_for_people += letter
+                    text_copy = text_copy[1:]
+                    print("Writer {0} wrote {1}.".format(self.name, letter))
+                    condition.acquire()
+                    condition.notify_all()
+                    condition.release()
+                    sleep(randint(*Writer.TIMEOUT_WRITING))
+                print("Writer {0} has wrote {1}.".format(self.name, string_for_people))
+                print('Writer {0} goes to sleep'.format(self.name))
+                string_for_people = ''
+            sleep(randint(*Writer.SLEEPING_TIME))
 
 
 NUMBER_OF_READERS = 5
 NUMBER_OF_WRITERS = 2
-TEXTS = ["Cъешь этих мягких французких булок", "Не ешь эти булки, они отравлены"]
+
 if __name__ == "__main__":
-    condition = Condition()
-    event = Event()
+    condition = Condition(lock=Lock())
+    lock = Lock()
     readers = [Faker().name() for _ in range(NUMBER_OF_READERS)]
     for ind, reader in enumerate(readers):
         Reader("{0} #{1}".format(reader, ind)).start()
-    while True:
-        for number in range(NUMBER_OF_WRITERS):
-            Writer("{0} #{1}".format(Faker().name(), number), TEXTS[randint(0, len(TEXTS) - 1)]).start()
-        with condition:
-            condition.notify_all()
+    for number in range(NUMBER_OF_WRITERS):
+        Writer("{0} #{1}".format(Faker().name(), number)).start()
