2062b702ee58a462b7bc39ff60098527dbd6561e
kirillsev1 <kirillprih47@gmail.com>
2022-12-22 00:45:23 +0300

    write tests

--- 1122/test_write.py
+++ 1122/test_write.py
@@ -2,13 +2,20 @@ import pytest
 from street import street, House
 import json
 
-test_write = [((1, 1, 12, 12, 'test_write'), 'test.json', '1', {"<street.House object at 0x7f671d32ff10>": "(1, 1, 12, 12, 'name')"})]
+test_write = [((1, 1, 12, 12, 'test_write'), 'test.json', '1', {"<House object>": "(1, 1, 12, 12, 'name')"})]
 
 
-@pytest.mark.parametrize('parameters, file_name, action, result', test_write)
-def file_write_check(parameters, file_name, action, result):
-    street(House(*parameters, file_name, action))
+@pytest.mark.parametrize('par, file_name, action, ans', test_write)
+def file_write_check(par, file_name, action, ans):
+    """Function which test writing to file.
+
+    Args:
+        par: tuple - tuple of parameters for class House.
+        file_name: str - name of json file.
+        action: str - what function must do.
+        ans: dict - dict of classes and parameters for tests.
+    """
+    street(House(*par), file_name, action)
     with open(file_name, 'r') as map_list:
         street_map = json.load(map_list)
-        assert street_map == result
-        
+        assert street_map[1] in ans.keys()
