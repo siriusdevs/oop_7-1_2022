b6adc31fe7bb7db7585ec32e34261982c457f524
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2022-12-26 16:31:21 +0300

    Update figures.py

--- figures.py
+++ figures.py
@@ -4,11 +4,11 @@ import math
 class Triangle:
     def __init__(self, sides):
         self.sides = sides
-    
+
     @property
     def sides(self):
         return self.__sides
-        
+
     @sides.setter
     def sides(self, sides):
         if len(sides) != 3:
@@ -17,20 +17,30 @@ class Triangle:
             side_a, side_b, side_c = float(sides[0]), float(sides[1]), float(sides[2])
         except:
             raise ValueError("Sides must be float or int")
-        if not (side_a + side_b > side_c and side_a + side_c > side_b and side_b + side_c > side_a):
+        if not (
+            side_a + side_b > side_c
+            and side_a + side_c > side_b
+            and side_b + side_c > side_a
+        ):
             raise Exception("Стороны не валидны")
         if side_a < 0 or side_b < 0 or side_c < 0:
             raise Exception("Отрицательных сторон не бывает челлл")
         else:
             self.__sides = [side_a, side_b, side_c]
-    
+
     def perimetr(self):
         return round(sum(self.sides), 2)
 
     def square(self):
         h_per = self.perimetr() / 2
-        side_a, side_b, side_c = float(self.sides[0]), float(self.sides[1]), float(self.sides[2])
-        return round(math.sqrt(h_per * (h_per - side_a) * (h_per - side_b) * (h_per - side_c)), 2)
+        side_a, side_b, side_c = (
+            float(self.sides[0]),
+            float(self.sides[1]),
+            float(self.sides[2]),
+        )
+        return round(
+            math.sqrt(h_per * (h_per - side_a) * (h_per - side_b) * (h_per - side_c)), 2
+        )
 
 
 class Circle:
@@ -40,7 +50,7 @@ class Circle:
     @property
     def radius(self):
         return self.__radius
-        
+
     @radius.setter
     def radius(self, radius):
         if not isinstance(radius, (float, int)):
@@ -50,8 +60,8 @@ class Circle:
         self.__radius = radius
 
     def len_circle(self):
-        return round(math.pi * self.radius ** 2, 2)
-    
+        return round(math.pi * self.radius**2, 2)
+
     def square(self):
         return round(2 * math.pi * self.radius, 2)
 
