68a41bce30ab7bfd36eea408cc811da56a8471e9
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2022-12-26 05:18:42 +0300

    Add files via upload

---
+++ lahta.py
@@ -0,0 +1,65 @@
+from random import randint
+from string import ascii_lowercase
+from threading import Condition, Lock, Thread
+from time import sleep
+
+
+class Writer(Thread):
+    
+    WRITE_TIME = 1
+    SLEEP_TIME = (1, 2)
+    CAN_WRITE = 5
+    
+    def __init__(self, name: str, book: Lock):
+        super().__init__()
+        self.name = name
+    
+    def write(self):
+        global notifier, book
+        book.acquire()
+        print(f"Writer {self.name} started rewriting")
+        with open("book.txt", "w+") as file:
+            file.write(f"By: {self.name}\n")
+        file.close()
+        for i in range(self.CAN_WRITE):
+            with open("book.txt",'a') as file:
+                with notifier:
+                    file.write(ascii_lowercase[randint(0, len(ascii_lowercase) - 1)])
+                    notifier.notify_all()
+            sleep(self.WRITE_TIME)
+        file.close()
+        book.release()
+        print(f"Writer {self.name} going to rest")
+            
+    
+    def run(self):
+        while True:
+            print(f"Writer {self.name} is resting")
+            sleep(randint(*Writer.SLEEP_TIME))
+            print(f"Writer {self.name} wanna work")
+            self.write()
+
+
+class Reader(Thread):
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        global notifier
+        while True:
+            with notifier:
+                notifier.wait()
+                with open("book.txt",'r') as file:
+                    print(f"Reader {self.name} reads \n{' '.join(file.readlines())}")
+
+if __name__ == "__main__":
+    NUM_WRITERS = 2
+    book = Lock()
+    notifier = Condition(Lock())
+    WRITERS = [Writer(str(num), book) for num in range(NUM_WRITERS)]
+    READERS = [Reader(str(num))for num in range(NUM_WRITERS)]
+    for i in range(len(WRITERS)):
+        WRITERS[i].start()
+        READERS[i].start()
+        sleep(1)
