afc54b4caf7e23404ad80b6dcf7de1d1e1dfc707
KabeSai <katnikita@vk.com>
2022-12-27 21:38:36 +0300

    fix workflows

--- HomeWork/HW_2/test_map.py
+++ HomeWork/HW_2/test_map.py
@@ -3,36 +3,42 @@ import pytest
 from map import *
 
 
-test_map1 = [('{}map_broken/'.format(Config().PWD), 'map', False), (Config().PWD, "map", True)]
+test_map1 = [('{}map_brok/'.format(Config(title_map="map").PWD), False), (Config(title_map="map").PWD, True)]
 
 
 @pytest.mark.xfail(raises=ImpossibleStructureMap)
-@pytest.mark.parametrize('PWD, map, exception', test_map1)
-def test_map(PWD, map, exception):
-    def valid(PWD):
+@pytest.mark.parametrize('PWD, exception', test_map1)
+def test_map(PWD, exception):
+
+    def ifvalid(pwd):
         try:
-            map_city = Map.get_cities(PWD)
+            map_city = Map.get_cities(pwd)
             if not isinstance(map_city, int):
                 if isinstance(map_city.cities, dict):
                     map_city.if_valid()
                     return True
             return False
-        except:
+        except BaseException:
             return False
-    assert valid(PWD) == exception
+    assert ifvalid(PWD) == exception
+
 
 test_house = [([5, 5, 10], True), ([3, "3", 4], False), ([-1, 9, 4], False), ([0, 9, 3], None)]
 
+
 @pytest.mark.xfail(raises=ImpossibleHouse)
 @pytest.mark.parametrize('arg, exception', test_house)
-def test_house(arg, exception):
+def test_house_f(arg, exception):
     assert House(*arg).if_valid() == exception
 
-test_city = [(["Tom", [10, 10]], True), (["and", [-1, 4]], False), (["Jery", [None, 4]], None), (["Pipe", [9.0, 3]], True)]
+
+test_city = [(["Tom", [10, 10]], True),
+             (["and", [-1, 4]], False),
+             (["Jerry", [None, 4]], None),
+             (["Pipe", [9.0, 3]], True)]
+
 
 @pytest.mark.xfail(raises=ImpossibleCity)
 @pytest.mark.parametrize('arg, exception', test_city)
-def test_house(arg, exception):
+def test_city_f(arg, exception):
     assert City(arg[0], arg[1]).if_valid() == exception
-
-
