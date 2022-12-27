d13b5d598358cee199189e4a1b4745d3f2c99e2a
Nirtkor <itnirtkor@gmail.com>
2022-12-27 13:32:30 +0300

    Cops visited philosopher's club

--- HW3/Philosophers.py
+++ HW3/Philosophers.py
@@ -1,10 +1,11 @@
-"""The module allows you to imagine a situation with philosophers at a round table (philosophers can reflect and consume food)."""
+"""The module allows you to imagine a situation with philosophers at a round table \
+    (philosophers can reflect and consume food)."""
 from threading import Thread, Lock
 from time import sleep
 from random import randint, choice
 
 NAMES_PHILOSOPHERS = ['Геродот', 'Евклид', 'Архимед', 'Альберт Андреевич', 'Вячеслав Александрович']
-EAT_TIME = (3, 4)
+EAT_TIME = (2, 3)
 THINK_TIME = (3, 4)
 TIMEOUT = 1
 
@@ -27,41 +28,35 @@ class ThinkingAndEating(Thread):
 
     def run(self):
         """Method for starting philosophers."""
-        print("{0} размышляет".format(self.name))
-        sleep(randint(*THINK_TIME))
-        print("{0} натолкнулся на мысль поесть".format(self.name))
-        self.eat()
+        while True:
+            print("{0} размышляет".format(self.name))
+            sleep(randint(*THINK_TIME))
+            print("{0} натолкнулся на мысль поесть".format(self.name))
+            self.eat()
 
     def eat(self):
         """Method for philosophers to eat."""
         left_chopstick = self.left_chopstick
         right_chopstick = self.right_chopstick
-        lock_left = left_chopstick.acquire(TIMEOUT)
-        lock_right = right_chopstick.acquire(TIMEOUT)
-        if lock_left:
+        if left_chopstick.acquire(TIMEOUT):
             print("{0} взял левую палочку".format(self.name))
-        if lock_right:
-            print("{0} взял правую палочку".format(self.name))
-        if lock_left and lock_right:
-            print("{0} приступил к поглощению".format(self.name))
-            sleep(randint(*EAT_TIME))
-            left_chopstick.release()
-            right_chopstick.release()
-            print("{0} поел и готов к размышлениям".format(self.name))
-        else:
-            if not lock_left:
-                print('{0} не смог взять левую палочку, поэтому вынужден размышлять'.format(self.name))
+            if right_chopstick.acquire(TIMEOUT):
+                print("{0} взял правую палочку".format(self.name))
+                print("{0} приступил к поглощению".format(self.name))
+                sleep(randint(*EAT_TIME))
                 left_chopstick.release()
-            if not lock_right:
-                print('{0} не смог взять правую палочку, поэтому вынужден размышлять'.format(self.name))
                 right_chopstick.release()
-            ThinkingAndEating.eat()
+                print("{0} поел и готов к размышлениям".format(self.name))
+            else:
+                print("{} положил на место левую палочку".format(self.name))
+                left_chopstick.release()
 
 
 if __name__ == "__main__":
     NUM_PHILOSOPHERS = len(NAMES_PHILOSOPHERS)
     CHOPSTICKS = [Lock() for _ in range(NUM_PHILOSOPHERS)]
-    PHILOSOPHERS = [ThinkingAndEating(str(num), CHOPSTICKS[num], CHOPSTICKS[(num + 1) % NUM_PHILOSOPHERS]) for num in range(NUM_PHILOSOPHERS)]
+    PHILOSOPHERS = [ThinkingAndEating(str(num), CHOPSTICKS[num], CHOPSTICKS[(num + 1) \
+        % NUM_PHILOSOPHERS]) for num in range(NUM_PHILOSOPHERS)]
     for philosopher in PHILOSOPHERS:
         philosopher.start()
         sleep(1)
