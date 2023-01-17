db23c31a5841e817dfa3dfa962d56df863f281ba
Nirtkor <itnirtkor@gmail.com>
2023-01-18 00:42:37 +0300

    Test for linter 2

--- HW5/writers_and_readers.py
+++ HW5/writers_and_readers.py
@@ -1,17 +1,27 @@
+"""Module for solving the problem about writers and readers."""
 from random import choice, randint
 from string import ascii_lowercase
 from threading import Condition, Lock, Thread
 from time import sleep
 
 class Writer(Thread):
+    """A class that creates a writer."""
+
     SLEEP_TIME = (1, 3)
     WRITE_TIME = 1
     def __init__(self, name):
+        """Initializing attributes for the writer.
+
+        Args:
+            name (str): the name of writer.
+        """
         super().__init__()
         self.name = name
     
     
     def write(self):
+        """A method for determining the spelling of something.
+        """
         global book, notifier, records
         book.acquire()
         print(f"Писатель {self.name} начал переписывать")
@@ -25,6 +35,8 @@ class Writer(Thread):
 
 
     def run(self):
+        """The method responsible for the writer's sleep and start.
+        """
         while True:
             print(f"Писатель {self.name} отдыхает")
             sleep(randint(*Writer.SLEEP_TIME))
@@ -33,12 +45,20 @@ class Writer(Thread):
     
     
 class Reader(Thread):
+    """A class that creates a reader."""
     def __init__(self, name):
+        """Initializing attributes for the reader.
+
+        Args:
+            name (str): the name of reader.
+        """
         super().__init__()
         self.name = name
 
     
     def run(self):
+        """The method responsible for the readers's start.
+        """
         global notifier, records
         while True:
             with notifier:
