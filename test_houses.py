cd8979f6aaf8614f209c1dd984267c87d74cb411
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-01-17 19:13:20 +0300

    Update test_houses.py

--- test_houses.py
+++ test_houses.py
@@ -5,22 +5,22 @@ from houses import Building
 @pytest.mark.xfail(raises=ValueError)
 def test0():
     """Function must raise ValueError."""
-    assert Building(0, 0, 0, 1)
+    assert Building(0, 0, 1)
 
 
 @pytest.mark.xfail(raises=ValueError)
 def test1():
     """Function must raise ValueError."""
-    assert Building(0, 0, 0, -1)
+    assert Building(0, 0, -1)
 
 
 @pytest.mark.xfail(raises=ValueError)
 def test2():
     """Function must raise ValueError."""
-    assert Building(0, 0, 0, 0)
+    assert Building(0, 0, 0)
 
 
 @pytest.mark.xfail(raises=ValueError)
 def test3():
     """Function must raise ValueError."""
-    assert Building(1, 0, -100, 51)
+    assert Building(0, -100, 51)
